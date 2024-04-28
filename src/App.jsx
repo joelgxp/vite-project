fda4a6482842966d43c31485ee2ec8d28ad2b8ce
Joel <joelvieirasouza@gmail.com>
2024-04-28 19:21:02 -0300

    Create configs

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import GlobalStyles from "./components/GlobalStyle"
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+function App() {
+
+  return (
+    <FundoGradiente>
+      <GlobalStyles />
+    </FundoGradiente>
+  )
+}
+
+export default App
